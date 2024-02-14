40532dc1ca3cf3883203f02dabb66be4f22889d9
kirank22 <45580946+kirank22@users.noreply.github.com>
2024-02-14 16:26:56 -0500

    midway

---
+++ BMI Calculator/Controllers/SecondViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SecondViewController.swift
+//  BMI Calculator
+//
+//  Created by Kiran Kothapalli on 1/24/24.
+//  Copyright © 2024 Angela Yu. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
